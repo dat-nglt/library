f17893fb574a35b1aaf3387804464200544a470e
dat-nglt <dat.nglt@gmail.com>
2024-04-29 01:35:21 +0700

    Thay đổi vào điều

--- views/user/profile.php
+++ views/user/profile.php
@@ -1,20 +1,44 @@
 <div class="profile">
-  <nav class="menu">
-    <div class="menu-option">Hồ sơ độc giả</div>
-    <div class="menu-option">Lịch sử mượn</div>
-  </nav>
-  <div class="content">
-    <div class="input-fields">
-      <label for=""></label>
-      <input type="text">
+  <div id="profile__title">	Hồ sơ độc giả</div>
+  <div class="profile__container">
+    <div class="profile__menu">
+      <div class="profile__menu-option profile__menu-option--active"> Thông tin cá nhân</div>
+      <div class="profile__menu-option"> Lịch sử mượn</div>
+      <div class="profile__menu-option"> Đổi mật khẩu</div>
     </div>
-    <div class="input-fields">
-      <label for=""></label>
-      <input type="text">
-    </div>
-    <div class="input-fields">
-      <label for=""></label>
-      <input type="text">
+    <div class="content">
+      <div class="input-fields">
+        <label for="">Mã độc giả</label>
+        <input type="text" value="2101364">
+      </div>
+      <div class="input-fields">
+        <label for="">Họ và tên</label>
+        <input type="text" value="Nguyễn Lê Tấn Đạt">
+      </div>
+      <div class="input-fields">
+        <label for="">Mã số sinh viên</label>
+        <input type="text" value="2101364">
+      </div>
+      <div class="input-fields">
+        <label for="">Ngày sinh</label>
+        <input type="text" value="20 / 04 / 2003">
+      </div>
+      <div class="input-fields">
+        <label for="">Số điện thoại</label>
+        <input type="text" value="0397364664">
+      </div>
+      <div class="input-fields">
+        <label for="">Địa chỉ Gmail</label>
+        <input type="text" value="dat.nglt@gmail.com">
+      </div>
+      <div class="input-fields">
+        <label for="">Số CCCD</label>
+        <input type="text" value="089203002653">
+      </div>
+      <div class="input-fields">
+        <label for="">Chi đoàn</label>
+        <input type="text" value="Kỹ thuật phần mềm">
+      </div>
     </div>
   </div>
 </div>
