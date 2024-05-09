101cf637324586f644a212d2b2566f2f667e51ad
TruongDat <truongvandat365@gmail.com>
2024-05-09 08:40:32 +0700

    home-update

--- views/user/home.php
+++ views/user/home.php
@@ -6,7 +6,6 @@ if (isset($data) && $data !== []) {
   $notificationType($notificationMessage, $notificationLink);
 }
 ?>
-
 <div class="home">
   <div class="user-search">
     <div class="user-search-sub">
@@ -23,6 +22,50 @@ if (isset($data) && $data !== []) {
       <button id="btn-search">Tìm kiếm</button>
     </div>
   </div>
+
+  <div class="book-data">
+    <div class="book-data-sub">
+      <div class="book-data-title">
+        <i class="fa-solid fa-tags"></i>Tài liệu chuyên ngành
+      </div>
+      <div class="book-data-content">
+        <a href="">- Toán cao cấp</a>
+        <a href="">- Pháp luật đại cương</a>
+        <a href="">- Tin học văn phòng</a>
+        <a href="">- Đại số tuyến tính</a>
+        <a href="" class="view-all">Xem tất cả</a>
+      </div>
+    </div>
+
+    <div class="book-data-sub">
+      <div class="book-data-title">
+        <i class="fa-solid fa-tags"></i>Tài liệu điện tử
+      </div>
+      <div class="book-data-content">
+        <a href="">- Toán cao cấp</a>
+        <a href="">- Pháp luật đại cương</a>
+        <a href="">- Tin học văn phòng</a>
+        <a href="">- Đại số tuyến tính</a>
+        <a href="" class="view-all">Xem tất cả</a>
+      </div>
+    </div>
+
+    <div class="book-data-sub">
+      <div class="book-data-title">
+        <i class="fa-solid fa-tags"></i>Tài liệu mới cập nhật
+      </div>
+      <div class="book-data-content">
+        <a href="">- Toán cao cấp</a>
+        <a href="">- Pháp luật đại cương</a>
+        <a href="">- Tin học văn phòng</a>
+        <a href="">- Đại số tuyến tính</a>
+        <a href="" class="view-all">Xem tất cả</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="book-news">
+    <div class="book-hot" style="border-right: 1px solid #333; padding: 5px 25px 0px 10px;">Điểm sách hay</div>
+    <div class="news-hot" style="padding: 5px 0px;">Tin tức - Event</div>
+  </div>
 </div>
-
-
